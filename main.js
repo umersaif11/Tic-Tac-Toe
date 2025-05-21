6b7970cd4bdfbc87dbfc89bc680e5492047f59fa
Muhammad Umar <muhammadumar.tudortmund@gmail.com>
2025-05-21 18:28:53 +0500

    Make droptoken function to play one round from each player after checking availability of cells on board.

--- main.js
+++ main.js
@@ -3,15 +3,38 @@ function GameBoard(){
     let rows = 3;
     let columns = 3;
     let board = [];
+
     //make 2D board of 3*3
     for(let i = 0; i < rows; i++){
         board[i] = [];
-        for(let i = 0; i < columns; i++){
+        for(let j = 0; j < columns; j++){
             board[i].push(Cell());
         }
     }
 
+    const getBoard = () => board;
 
+    //function to play each round by players
+    const dropToken = (row, col, playerToken) => {
+
+        //first check availability of cells in board
+        const availableCells = board.map((row) => 
+        row.filter((cell) => cell.getValue() === 0)
+        ).flat();
+
+        //if no cells then stop execution
+        if(!availableCells.length) return;
+
+        //if there are available cell then assign token
+        board[row][col].addToken(playerToken);
+
+    }
+
+    return {
+        getBoard,
+        dropToken,
+
+    }
 }
 function Cell(){
     let value = 0;
@@ -27,3 +50,6 @@ function Cell(){
         getValue
     };
 }
+
+let game = GameBoard();
+game.dropToken(0,2,'X');
